9e7b5ccbd95ab0accc9067569c022ae10b2a8ea5
Jeremy J. Lee <doctorslow@gmail.com>
2018-04-16 22:45:25 -0700

    table display and initial functions set up

--- jamazonCustomer.js
+++ jamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var colors = require("colors");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -11,16 +12,30 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    start();
+    console.log("connection successful!");
+    customerPrompt();
 });
 
-function start() {
+function customerPrompt() {
     inquirer.prompt({
         name: "action",
         type: "list",
-        message: "Welcome to Jamazon--your one-stop shop for 'J & A's Erotic Jammery & Chutneria'. Choose 'Shop' below to get started.",
-        choices: [
-            "Shop", "Quit"
-        ]
+        message: "    * * * * * \nWelcome to Jamazon--your one-stop shop for 'J & A's Erotic Jammery & Chutneria'. \n     * * * * * \nChoose 'Shop' below to get started or 'Quit' to exit.\n     * * * * * ".blue,
+        choices: ["Shop", "Quit"]
+    }).then(function (user) {
+        if (user.action == "Shop") {
+            tableDisplay();
+        } else {
+            process.exit();
+        }
     })
-}
+};
+
+function tableDisplay() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " || " + res[i].product_name + " || " + res[i].department_name + " || " + res[i].price + " || " + res[i].stock_quantity + "\n");
+        }
+        // purchase(res);
+    })
+};
