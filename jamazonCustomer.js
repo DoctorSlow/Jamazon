272713c73fd7dfe343a8352959c544e0d811252e
Jeremy J. Lee <doctorslow@gmail.com>
2018-04-17 21:28:52 -0700

    customer.js working, but not updating quantities

--- jamazonCustomer.js
+++ jamazonCustomer.js
@@ -36,6 +36,45 @@ function tableDisplay() {
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].item_id + " || " + res[i].product_name + " || " + res[i].department_name + " || " + res[i].price + " || " + res[i].stock_quantity + "\n");
         }
-        // purchase(res);
+        purchase(res);
     })
 };
+
+function purchase(res) {
+    inquirer.prompt([{
+        name: "choice",
+        type: "input",
+        message: "Please enter the product name or ID number that you would like to buy"
+    }]).then(function (answer) {
+        var correct = false;
+        for (var i = 0; i < res.length; i++) {
+            if (res[i].product_name == answer.choice) {
+                correct = true;
+                var product = answer.choice;
+                var id = i;
+                inquirer.prompt({
+                    name: "quantity",
+                    type: "input",
+                    message: "Please indicate how many jars you would like to purchase.",
+                    validate: function (value) {
+                        if (isNaN(value) == false) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }).then(function (answer) {
+                    if ((res[id].stock_quantity - answer.quantity) > 0) {
+                        connection.query("UPDATE products SET stock_quantity =' " + (res[id].stock_quantity - answer.quantity) + " ' WHERE product_name = ' " + product + " ' ", function (err, res2) {
+                            console.log("Success. Thank you for your purchase.");
+                            tableDisplay();
+                        })
+                    } else {
+                        console.log("Insufficient stock available. Please try a different product or quantity");
+                        customerPrompt(res);
+                    }
+                })
+            }
+        }
+    })
+}
